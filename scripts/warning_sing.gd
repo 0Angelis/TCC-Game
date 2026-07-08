7d9b8ac5cb6b4bf33e9e91cd03033555ba910758
Felipe Angelis <felipeangelis26@hotmail.com>
2026-07-08 19:54:44 -0300

    textos

--- scripts/warning_sing.gd
+++ scripts/warning_sing.gd
@@ -1,28 +1,21 @@
 extends Node2D
 
-@onready var texture = $texture
+@onready var alert_sign = $alert_sign
 @onready var area_sign = $area_sign
 
-const lines: Array[String] = [
-	"Olá, aventureiro!",
-	"É muito bom vê-lo por aqui.",
-	"Espero que esteja preparado...",
-	"Sua jornada está apenas...",
-	"...COMEÇANDO!"
-]
+@export var dialog_texts : Array[String]
 
 func _unhandled_input(event):
 
 	if area_sign.get_overlapping_bodies().size() > 0:
 
-		texture.show()
+		alert_sign.show()
 
 		if event.is_action_pressed("interact") and !DialogManager.is_message_active:
 
-			texture.hide()
-
-			DialogManager.start_message(global_position, lines)
+			alert_sign.hide()
+			DialogManager.start_message(global_position, dialog_texts)
 
 	else:
 
-		texture.hide()
+		alert_sign.hide()
