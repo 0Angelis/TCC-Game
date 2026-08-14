f00212063ac165d52c474832c77c262fa4f64e54
Felipe Angelis <felipeangelis26@hotmail.com>
2026-08-14 09:13:34 -0300

    melhorias

--- scripts/warning_sing.gd
+++ scripts/warning_sing.gd
@@ -3,19 +3,21 @@ extends Node2D
 @onready var alert_sign = $alert_sign
 @onready var area_sign = $area_sign
 
-@export var dialog_texts : Array[String]
+@export var dialog_texts: Array[String]
+
+func _ready():
+	alert_sign.hide()
+
+
+func _process(_delta):
+	if area_sign.get_overlapping_bodies().size() > 0:
+		alert_sign.show()
+	else:
+		alert_sign.hide()
+
 
 func _unhandled_input(event):
-
 	if area_sign.get_overlapping_bodies().size() > 0:
-
-		alert_sign.show()
-
 		if event.is_action_pressed("interact") and !DialogManager.is_message_active:
-
 			alert_sign.hide()
 			DialogManager.start_message(global_position, dialog_texts)
-
-	else:
-
-		alert_sign.hide()
